72225a95396a5ca588c04f9c3b00cf4d2736a873
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-09 18:54:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,7 +98,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      GROUP BY id
+      GROUP BY student.id
       LIMIT 1;
     SQL
     DB[:conn].execute(sql).collect do |row|
