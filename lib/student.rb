e0aa91f518d1b699893f8a9df845722d98f80916
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-09 18:18:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,13 +62,20 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(*)
+      SELECT *
       FROM students
       WHERE grade = 9;
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+
+      
+    
   end
 
 
