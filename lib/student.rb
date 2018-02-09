12baa811db46cf40c3c33c65c0ee671b83585906
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-09 18:30:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,16 +72,27 @@ class Student
   end
 
   def self.students_below_12th_grade
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade < 12
+      SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < 12
+      WHERE grade = 10
+      LIMIT number;
     SQL
-  DB[:conn].execute(sql).collect do |row|
-    self.new_from_db(row)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
-end
-
 
 
 
